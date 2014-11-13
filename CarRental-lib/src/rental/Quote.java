d33a018400be3c6666e557e67e8e18fe46129805
Bavo Goosens <bavo.goosens@gmail.com>
2014-11-13 13:26:36 +0100

    quote shizzle

--- CarRental-lib/src/rental/Quote.java
+++ CarRental-lib/src/rental/Quote.java
@@ -2,10 +2,16 @@ package rental;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import static javax.persistence.TemporalType.DATE;
 
+@MappedSuperclass
 public class Quote implements Serializable {
 
+    @Temporal(DATE)
     private Date startDate;
+    @Temporal(DATE)
     private Date endDate;
     private String carRenter;
     private String rentalCompany;
